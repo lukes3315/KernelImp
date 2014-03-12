23c95f561be045edf9e7491a3578e5183b19fe9e
mc-kenn_l <lucas.mckenna@mobile-devices.fr>
2014-03-12 14:49:51 +0100

    Added crop() scale() // basic though should still be improved (nearest neighbour)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <opencv2/opencv.hpp>
+#include <stdio.h>
 #include <iostream>
+
 
 #define KERNEL_SIZE 3
 #define IDENTITY 0
@@ -7,9 +9,69 @@
 #define EDGE_DETECTION_1 2
 #define EDGE_DETECTION_2 3
 #define EDGE_DETECTION_3 4
+#define BLUR 5
 
+#define KERNEL_WANTED SHARPENING
 
-#define KERNEL_WANTED EDGE_DETECTION_2
+#define R_2_GRAY 0.2126
+#define G_2_GRAY 0.7152
+#define B_2_GRAY 0.0722
+
+int divisor = 1;
+
+int getGrayPixel(int R, int G, int B);
+void convertRGBToGray(cv::Mat & from, cv::Mat & to);
+int createPixelColor(int R, int G, int B);
+void setPixelColor(int x, int y, cv::Mat & img, int color);
+int getPixelColor(int x, int y, cv::Mat & img);
+int getRed(int color);
+int getGreen(int color);
+int getBlue(int color);
+
+int getGrayPixel(int R, int G, int B)
+{
+  int gray = R*R_2_GRAY + G*G_2_GRAY + B*B_2_GRAY;
+  return gray;
+}
+
+void convertRGBToGray(cv::Mat & from, cv::Mat & to)
+{
+  if (to.data == NULL)
+    to.create(from.rows, from.cols, 1);
+
+  for (int y = 0; y < from.rows ; ++y)
+    {
+      for (int x = 0 ; x < from.cols ; ++x)
+	{
+	  int color = getPixelColor(x, y, from);
+	  to.data[y * from.cols + x] = 
+	    getGrayPixel(getRed(color), getGreen(color), getBlue(color));
+	}
+    }
+}
+
+void split(cv::Mat & from, cv::Mat & toR, cv::Mat & toG, cv::Mat & toB)
+{
+  if (toR.data == NULL)
+    toR.create(from.rows, from.cols, 1);
+
+  if (toG.data == NULL)
+    toG.create(from.rows, from.cols, 1);
+
+  if (toB.data == NULL)
+    toB.create(from.rows, from.cols, 1);
+
+  for (int y = 0 ; y < from.rows ; ++y)
+    {
+      for (int x = 0 ; x < from.cols ; ++x)
+	{
+	  int color = getPixelColor(x, y, from);
+	  toR.data[y * from.cols + x] = getRed(color);
+	  toG.data[y * from.cols + x] = getGreen(color);
+	  toB.data[y * from.cols + x] = getBlue(color);
+	}
+    }
+}
 
 int createPixelColor(int R, int G, int B)
 {
@@ -66,7 +128,104 @@ int get_X(int x, int curr_index)
   return 0;
 }
 
-int getAverage(int x, int y, cv::Mat & img, char kernel[][3])
+void copy(cv::Mat & from, cv::Mat & to)
+{
+  for (int i = 0 ; i < (to.cols * to.rows) * to.channels() ; ++i)
+    {
+      to.data[i] = from.data[i];
+    }
+}
+
+void scale(cv::Mat & from, cv::Mat & to, float _scale)
+{
+  int _n_rows = from.rows * _scale;
+  int _n_cols = from.cols * _scale;
+
+  if (to.data == NULL)
+    {
+      to.create(_n_rows, _n_cols, from.type());
+    }
+  else if (to.rows != _n_rows
+	   || to.cols != _n_cols)
+    {
+      to.release();
+      to.create(_n_rows, _n_cols, from.type());
+    }
+  if (_scale > 1.f || _scale < 1.f)
+    {
+      for (int y = 0 ; y < to.rows ; ++y)
+	{
+	  for (int x = 0 ; x < to.cols ; ++x)
+	    {
+	      setPixelColor(x, y, to,
+			    getPixelColor((int)x/_scale, (int)y/_scale, from));
+	    }
+	}
+    }
+  else if (_scale == 1.f)
+    {
+      copy(from, to);
+    }
+  std::cout << _scale<<std::endl;
+}
+
+int getY(int64_t coords)
+{
+  return coords & 0xFFFFFFFF;
+}
+
+int getX(int64_t coords)
+{
+  return (coords >> 32);
+}
+
+int64_t getCoords(int x, int y)
+{
+  int64_t coords = 0;
+
+  coords = x;//coords = x
+  coords <<= 32;//push 32 '0' into 64bit integer so x value located at the begining
+  coords |= y;//apply OR mask to add y value to 64bit long integer
+  return coords;
+}
+
+void crop(cv::Mat & from, cv::Mat & to, int64_t start_coords, int64_t end_coords)
+{
+  int x = getX(start_coords);
+  int y = getY(start_coords);
+  int _end_x = getX(end_coords);
+  int _end_y = getY(end_coords);
+
+  std::cout << "x " << x << std::endl;
+  std::cout << "y " << y << std::endl;
+  std::cout << "endx " << _end_x << std::endl;
+  std::cout << "endy " << _end_y << std::endl;
+  int cols = _end_x - x;
+  int rows = _end_y - y;
+
+  if (to.data == NULL)
+    to.create(rows, cols, from.type());
+  if (to.cols != cols
+      || to.rows != rows)
+    {
+      to.release();
+      to.create(rows, cols, from.type());
+    }
+  else
+    {
+      int _crop_x = 0, _crop_y = 0;
+      for (int _y = y; _y < _end_y ; ++_y && ++_crop_y)
+	{
+	  _crop_x = 0;
+	  for (int _x = x ; _x < _end_x ; ++_x && ++_crop_x)
+	    {
+	      setPixelColor(_crop_x, _crop_y, to, getPixelColor(_x, _y, from));
+	    }
+	}
+    }
+}
+
+int applyKernel(int x, int y, cv::Mat & img, char kernel[][3])
 {
   int avg_R = 0;
   int avg_G = 0;
@@ -76,9 +235,9 @@ int getAverage(int x, int y, cv::Mat & img, char kernel[][3])
   int curr_x = 0;
   int color = 0;
   
-  for (int i = 0 ; i < 3 ; ++i)
+  for (int i = 0 ; i < KERNEL_SIZE ; ++i)
     {
-      for (int j = 0 ; j < 3 ; ++j)
+      for (int j = 0 ; j < KERNEL_SIZE ; ++j)
 	{
 	  if (i == 0)
 	    {
@@ -103,6 +262,12 @@ int getAverage(int x, int y, cv::Mat & img, char kernel[][3])
 	  avg_B += getBlue(color) * kernel[i][j];
 	}
     }
+  if (divisor > 1)
+    {
+      avg_R /= divisor;
+      avg_G /= divisor;
+      avg_B /= divisor;
+    }
   if (avg_R < 0)
     avg_R = 0;
   else if (avg_R > 255)
@@ -121,33 +286,37 @@ int getAverage(int x, int y, cv::Mat & img, char kernel[][3])
 
 int main(void)
 {
-  cv::VideoCapture cap(0);
+  cv::VideoCapture cap("video.mp4");
   int key = -1;
   cv::Mat img;
   cv::Mat img2;
+  cv::Mat gray;
+  cv::Mat scaling;
+  cv::Mat scaling_sharp;
+  cv::Mat cropped;
 
   if (!cap.isOpened())
     return -1;
-
   char kernel[3][3];
   memset(kernel[0], 0, 3);
   memset(kernel[1], 0, 3);
   memset(kernel[2], 0, 3);
-
 #if KERNEL_WANTED == SHARPENING
-  kernel[0][0] = 0;
+  kernel[0][0] = -1;
   kernel[0][1] = -1;
-  kernel[0][2] = 0;
+  kernel[0][2] = -1;
   kernel[1][0] = -1;
-  kernel[1][1] = 5;
+  kernel[1][1] = 9;
   kernel[1][2] = -1;
-  kernel[2][0] = 0;
+  kernel[2][0] = -1;
   kernel[2][1] = -1;
-  kernel[2][2] = 0;
+  kernel[2][2] = -1;
   std::cout << "Sharpen kernel" << std::endl;
+  divisor = 1;
 #elif KERNEL_WANTED == IDENTITY
   kernel[1][1] = 1;
   std::cout << "Original kernel" << std::endl;
+  divisor = 1;
 #elif KERNEL_WANTED == EDGE_DETECTION_1
   kernel[0][1] = 1;
   kernel[1][0] = 1;
@@ -155,6 +324,7 @@ int main(void)
   kernel[1][2] = 1;
   kernel[2][1] = 1;
   std::cout << "Edge detection #1 kernel" << std::endl;
+  divisor = 1;
 #elif KERNEL_WANTED == EDGE_DETECTION_2
   memset(kernel[0], -2, KERNEL_SIZE);
   memset(kernel[1], -2, KERNEL_SIZE);
@@ -165,173 +335,56 @@ int main(void)
   kernel[2][2] = 1;
   kernel[1][1] = 4;
   std::cout << "Edge detection #2 kernel" << std::endl;
+  divisor = 1;
 #elif KERNEL_WANTED == EDGE_DETECTION_3
   memset(kernel[0], -1, KERNEL_SIZE);
   memset(kernel[1], -1, KERNEL_SIZE);
   memset(kernel[2], -1, KERNEL_SIZE);
   kernel[1][1] = 8;
   std::cout << "Edge detection #3 kernel" << std::endl;
+  divisor = 1;
+#elif KERNEL_WANTED == BLUR
+  memset(kernel[0], 1, KERNEL_SIZE);
+  memset(kernel[1], 1, KERNEL_SIZE);
+  memset(kernel[2], 1, KERNEL_SIZE);
+  std::cout << "Blur kernel" << std::endl;
+  divisor = 9;
 #endif
+  getCoords(100, 20);
 
-
-  unsigned char * data = NULL;
-
-
-  cv::Vec3f colors;
   while (key < 0)
     {
       cap >> img;
       if (img.empty())
 	break;
-      if (data == NULL)
+      if (img2.data == NULL)
 	{
-	  data = new unsigned char[(img.cols * img.rows) * img.channels()];
 	  img2.create(img.rows, img.cols, img.type());
+	  //scaling.create(img.rows, img.cols, img.type());
 	}
+      crop(img, cropped, getCoords(100, 100), getCoords(200, 200));
+      scale(cropped, scaling, 2);
+      convertRGBToGray(img, gray);
+      
+      /*
       for (int y = 1 ; y < (img.rows) - 1; ++y)
 	{
 	  for (int x = 1 ; x < (img.cols) - 1 ; ++x)
 	    {
-	      
-	      
-	      setPixelColor(x, y, img2, getAverage(x, y, img, kernel));
-	      //setPixel(x, y, img, 0xFFFFFF);
-
-	      //char pixels[3][9];
-	      /*
-	      colors = img.at<cv::Vec3f>(y - 1, x - 1);
-	      avg_R += kernel[0][0] * colors.val[0];
-	      avg_G += kernel[0][0] * colors.val[1];
-	      avg_B += kernel[0][0] * colors.val[2];
-
-	      colors = img.at<cv::Vec3f>(y - 1, x);
-	      avg_R += kernel[0][1] * colors.val[0];
-	      avg_G += kernel[0][1] * colors.val[1];
-	      avg_B += kernel[0][1] * colors.val[2];
-	      
-	      colors = img.at<cv::Vec3f>(y - 1, x + 1);
-	      
-	      avg_R += kernel[0][2] * colors.val[0];
-	      avg_G += kernel[0][2] * colors.val[1];
-	      avg_B += kernel[0][2] * colors.val[2];
-
-	      colors = img.at<cv::Vec3f>(y, x - 1);
-	      avg_R += kernel[1][0] * colors.val[0];
-	      avg_G += kernel[1][0] * colors.val[1];
-	      avg_B += kernel[1][0] * colors.val[2];
-
-	      colors = img.at<cv::Vec3f>(y, x);
-	      avg_R += kernel[1][1] * colors.val[0];
-	      avg_G += kernel[1][1] * colors.val[1];
-	      avg_B += kernel[1][1] * colors.val[2];
-	      
-	      colors = img.at<cv::Vec3f>(y, x + 1);
-	      avg_R += kernel[1][2] * colors.val[0];
-	      avg_G += kernel[1][2] * colors.val[1];
-	      avg_B += kernel[1][2] * colors.val[2];
-
-	      colors = img.at<cv::Vec3f>(y - 1, x - 1);
-	      avg_R += kernel[2][0] * colors.val[0];
-	      avg_G += kernel[2][0] * colors.val[1];
-	      avg_B += kernel[2][0] * colors.val[2];
-
-	      colors = img.at<cv::Vec3f>(y + 1, x);
-	      avg_R += kernel[2][1] * colors.val[0];
-	      avg_G += kernel[2][1] * colors.val[1];
-	      avg_B += kernel[2][1] * colors.val[2];
-
-	      colors = img.at<cv::Vec3f>(y + 1, x + 1);
-	      avg_R += kernel[2][2] * colors.val[0];
-	      avg_G += kernel[2][2] * colors.val[1];
-	      avg_B += kernel[2][2] * colors.val[2];
-
-	      avg_R /= 9;
-	      avg_G /= 9;
-	      avg_B /= 9;
-
-	      cv::Vec3f t;
-	      t.val[0] = avg_R;
-	      t.val[1] = avg_G;
-	      t.val[2] = avg_B;
-	      */
-	      //img2.data[(y*img2.channels()) * img2.cols + (x * img2.channels())] = 255;
-	      //img2.data[(y*img2.channels()) * img2.cols + (x * img2.channels()) + 1] = 255;
-	      //img2.data[(y*img2.channels()) * img2.cols + (x * img2.channels()) + 2] = 255;
-	      /*
-	      cv::Vec3f t;
-	      t.val[0] = 255;
-	      t.val[1] = 255;
-	      t.val[2] = 255;
-	      //t.val[3] = 255;
-	      img2.at<cv::Vec3f>(y, x) = t;*/
-
-
-
-
-	      /*
-	      //Top row pixels      
-	      pixels[0][0] = img.data[(y - 3) * img.cols + (x - 3)];
-	      pixels[0][1] = img.data[(y - 3) * img.cols + (x - 2)];
-	      pixels[0][2] = img.data[(y - 3) * img.cols + (x - 1)];
-
-	      pixels[0][3] = img.data[(y - 3) * img.cols + (x)];
-	      pixels[0][4] = img.data[(y - 3) * img.cols + (x + 1)];
-	      pixels[0][5] = img.data[(y - 3) * img.cols + (x + 2)];
-
-	      pixels[0][6] = img.data[(y - 3) * img.cols + (x + 3)];
-	      pixels[0][7] = img.data[(y - 3) * img.cols + (x + 4)];
-	      pixels[0][8] = img.data[(y - 3) * img.cols + (x + 5)];
-
-	      //middle row pixels
-	      pixels[1][0] = img.data[(y) * img.cols + (x - 3)];
-	      pixels[1][1] = img.data[(y) * img.cols + (x - 2)];
-	      pixels[1][2] = img.data[(y) * img.cols + (x - 1)];
-
-	      pixels[1][3] = img.data[(y) * img.cols + (x)];
-	      pixels[1][4] = img.data[(y) * img.cols + (x + 1)];
-	      pixels[1][5] = img.data[(y) * img.cols + (x + 2)];
-
-	      pixels[1][6] = img.data[(y) * img.cols + (x + 3)];
-	      pixels[1][7] = img.data[(y) * img.cols + (x + 4)];
-	      pixels[1][8] = img.data[(y) * img.cols + (x + 5)];
-
-	      //Bottom row pixels
-	      pixels[2][0] = img.data[(y + 3) * img.cols + (x - 3)];
-	      pixels[2][1] = img.data[(y + 3) * img.cols + (x - 2)];
-	      pixels[2][2] = img.data[(y + 3) * img.cols + (x - 1)];
-
-	      pixels[2][3] = img.data[(y + 3) * img.cols + (x)];
-	      pixels[2][4] = img.data[(y + 3) * img.cols + (x + 1)];
-	      pixels[2][5] = img.data[(y + 3) * img.cols + (x + 2)];
-
-	      pixels[2][6] = img.data[(y + 3) * img.cols + (x + 3)];
-	      pixels[2][7] = img.data[(y + 3) * img.cols + (x + 4)];
-	      pixels[2][8] = img.data[(y + 3) * img.cols + (x + 5)];
-
-	      int avg_R = 0, avg_G = 0, avg_B = 0;
-
-	      avg_R = (pixels[0][0] * kernel[0][0]) + (pixels[0][3] * kernel[0][1]) + (pixels[0][6] * kernel[0][2]);
-	      avg_G = (pixels[0][1] * kernel[0][0]) + (pixels[0][4] * kernel[0][1]) + (pixels[0][7] * kernel[0][2]);
-	      avg_B = (pixels[0][2] * kernel[0][0]) + (pixels[0][5] * kernel[0][1]) + (pixels[0][8] * kernel[0][2]);
-
-	      avg_R += (pixels[1][0] * kernel[1][0]) + (pixels[1][3] * kernel[1][1]) + (pixels[1][6] * kernel[1][2]);
-	      avg_G += (pixels[1][1] * kernel[1][0]) + (pixels[1][4] * kernel[1][1]) + (pixels[1][7] * kernel[1][2]);
-	      avg_B += (pixels[1][2] * kernel[1][0]) + (pixels[1][5] * kernel[1][1]) + (pixels[1][8] * kernel[1][2]);
-
-	      avg_R += (pixels[2][0] * kernel[2][0]) + (pixels[2][3] * kernel[2][1]) + (pixels[2][6] * kernel[2][2]);
-	      avg_G += (pixels[2][1] * kernel[2][0]) + (pixels[2][4] * kernel[2][1]) + (pixels[2][7] * kernel[2][2]);
-	      avg_B += (pixels[2][2] * kernel[2][0]) + (pixels[2][5] * kernel[2][1]) + (pixels[2][8] * kernel[2][2]);
-
-	      avg_R /= 9;
-	      avg_G /= 9;
-	      avg_B /= 9;
-	      */
+	      setPixelColor(x, y, img2, applyKernel(x, y, img, kernel));
 	    }
 	}
+      */
+      cv::imshow("Cropped", cropped);
+      cv::imshow("Scaled", scaling);
+      cv::imshow("Gray", gray);
       cv::imshow("VideoBase", img);
-      cv::imshow("Video", img2);
+      //cv::imshow("Video", img2);
       key = cv::waitKey(1);
     }
-
+  img.release();
+  img2.release();
+  gray.release();
+  cap.release();
   return 0;
 }
